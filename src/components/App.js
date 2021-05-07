8b18fd7ddff9b85cac06f64143c8330d69358ae0
colineckert <colineckert10@gmail.com>
2021-05-06 21:36:10 -0700

    add transactions

--- src/components/App.js
+++ src/components/App.js
@@ -14,40 +14,54 @@ const fetchPrices = () => {
 
 export default function App() {
   
-  const [count, setCount] = useState(0);
+  const [funds, setFunds] = useState(1000000);
   const [time, setTime] = useState({});
   const [bpi, setBpi] = useState([]);
 
   useEffect(() => {
     fetchPrices().then(({time, bpi}) => {
         setTime(time);
-        setBpi([bpi.EUR, bpi.USD]);
+        setBpi([bpi.USD]);
       });
-  }, [])
+  }, []);
+
+  const buyOrder = () => {
+    setFunds(Math.floor(funds - bpi[0].rate_float));
+  }
+
+  const sellOrder = () => {
+    setFunds(Math.floor(funds + bpi[0].rate_float));
+  }
 
   return (
     <div>
       <h1>Coinbase Practice App</h1>
       <div>
-        <button onClick={() => setCount(count + 1)}>+</button>
-        <p>{count}</p>
-      </div>
-      <div>
-        <h2>Bitcoin</h2>
-        <button onClick={fetchPrices}>Fetch Latest Prices</button>
         <div>
-          <p>{time.updated}</p>
-          <ul>
-            {bpi.map((coin, idx) => {
-                return <li key={idx}>{coin.code}: {coin.rate}</li>
-            })}
-          </ul>
+          <h2>Colin Eckert</h2>
+          <h3>Funds: ${funds.toLocaleString()}</h3>
+        </div>
+        <div>
+          <h2>Bitcoin Rates</h2>
+          <div>
+            <p>Last Updated: {time.updated}</p>
+            <ul>
+              {bpi.map((coin, idx) => {
+                  return <li key={idx}>{coin.code}: ${coin.rate_float.toFixed(2)}</li>
+              })}
+            </ul>
+          </div>
         </div>
       </div>
-      <h2>List Practice</h2>
-      <ul>
-        <li>Test</li>
-      </ul>
+      <div>
+        <h2>Transactions</h2>
+        <button onClick={() => buyOrder()}>Buy</button>
+        <button onClick={() => sellOrder()}>Sell</button>
+        <ul>
+          <li>Buy</li>
+          <li>Sell</li>
+        </ul>
+      </div>
     </div>
   )
 }
