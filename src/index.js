d92689d0dbcf0a4235a4eabc7d3caa6c130e9d40
colineckert <colineckert10@gmail.com>
2021-05-06 23:04:10 -0700

    add reset css file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './components/root';
+import './index.css';
+// import './reset.css';
 
 document.addEventListener('DOMContentLoaded', () => {
   const rootEl = document.getElementById('root');
